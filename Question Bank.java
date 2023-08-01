dc825da6e564a54a84f3128bea28ac68fdbbf951
Yash Singh <122292102+Yash3148@users.noreply.github.com>
2023-08-01 23:11:07 +0530

    First Commit

---
+++ Question Bank.java
@@ -0,0 +1,21 @@
+Ques1. Print the pattern
+*
+**
+***
+****
+Answer:-
+package com.apnacollege;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.print("****");
+        }
+     }
+//output ---> *
+//            **
+//            ***
+//            ****
+--------------------------------------------------------------------------------------------------------------------------------
