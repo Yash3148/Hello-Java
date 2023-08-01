37fa19ca20339c6691b36dd4af8a0f0308e9d605
Yash Singh <122292102+Yash3148@users.noreply.github.com>
2023-08-02 01:07:34 +0530

    First Commit

---
+++ add-sub.java
@@ -0,0 +1,13 @@
+package com.apnacollege;
+
+public class Main {
+    public static void main(String[] args) {
+        //variables
+        int a = 25;
+        int b = 10;
+        int sum = a + b;
+        int diff = a - b;
+        System.out.print(sum);
+        System.out.print(diff);
+        }
+     }
