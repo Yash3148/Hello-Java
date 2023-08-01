4a7b9f757092cbd6ebe456591cb0ef5249fa3990
Yash <yashsingh3148@gmail.com>
2023-08-01 20:36:39 +0530

    first commit

---
+++ hello world.java
@@ -0,0 +1,7 @@
+class FirstClass {
+    public static void main(string args[]) {
+        system.out.println("Hello Linux");
+        system.out.println("Hello Linux");
+        system.out.println("Hello Linux");
+    }
+}
