e864d5f0928f0470c8dabf5f6a2c3e5751b7e914
Yash Singh <122292102+Yash3148@users.noreply.github.com>
2023-08-02 01:17:50 +0530

    First Commit

---
+++ multiplication.java
@@ -0,0 +1,12 @@
+package com.apnacollege;
+
+public class Main {
+    public static void main(String[] args) {
+        //variables
+        int a = 25;
+        int b = 10;
+        int mul = a * b
+        System.out.print(mul);
+    }
+}
+// output ---> 250
