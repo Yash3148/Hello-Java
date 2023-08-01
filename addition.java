d7c4efb10fbdb796764e49f35f14087508e76b75
Yash Singh <122292102+Yash3148@users.noreply.github.com>
2023-08-02 01:03:34 +0530

    First Commit

---
+++ addition.java
@@ -0,0 +1,13 @@
+package com.apnacollege;
+
+public class Main {
+    public static void main(String[] args) {
+        //variables
+        int a = 25;
+        int b = 10;
+        int sum = a + b;
+        System.out.print(sum);
+        }
+     }
+
+// output ---> 35
