bb91942abcab2ceb05a0f66bfd0ba0bd4a75b94f
Yash Singh <122292102+Yash3148@users.noreply.github.com>
2023-08-01 21:53:31 +0530

    First Commit

--- hello-worldln.java
+++ hello-worldln.java
@@ -1,8 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
-        System.out.println("Hello Linux!!");
+        System.out.print("Hello Linux!!");
         }
      }
 // output ---> Hello World!!
 //             Hello Linux!!
+
+package com.apnacollege;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello World!!\n");
+        System.out.print("Hello Linux!!\n");
+        }
+     }
+// \n is a special character sequence known as the "newline" or "line feed."
