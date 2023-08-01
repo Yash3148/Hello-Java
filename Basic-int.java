d95d955c2630e59b3c5cd52907a0970e6bfdb913
Yash Singh <122292102+Yash3148@users.noreply.github.com>
2023-08-02 01:16:39 +0530

    Basic-int.java

---
+++ Basic-int.java
@@ -0,0 +1,50 @@
+package com.apnacollege;
+
+public class Main {
+    public static void main(String[] args) {
+        //variables
+        int a = 25;
+        int b = 10;
+        int sum = a + b;
+        int diff = a - b;
+        System.out.print(sum);
+        System.out.print(diff);
+        }
+     }
+// output ---> 3515
+---------------------------------------------------------------------------------------------
+package com.apnacollege;
+
+public class Main {
+    public static void main(String[] args) {
+        //variables
+        int a = 25;
+        int b = 10;
+        int sum = a + b;
+        int diff = a - b;
+        System.out.println(sum);
+        System.out.print(diff);
+        }
+     }
+// output ---> 35
+//             15
+----------------------------------------------------------------------------------------------
+package com.apnacollege;
+
+public class Main {
+    public static void main(String[] args) {
+        //variables
+        int a = 25;
+        int b = 10;
+        int sum = a + b;
+        int diff = a - b;
+        int mul = a * b;
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(mul);
+    }
+}
+// output ---> 35
+//             15
+//             250
+----------------------------------------------------------------------------------------------
