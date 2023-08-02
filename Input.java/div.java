0a881098cd6dec6a77b6897e9c8168cbae7de8ff
Yash Singh <122292102+Yash3148@users.noreply.github.com>
2023-08-02 11:26:21 +0530

    Create div.java

---
+++ Input.java/div.java
@@ -0,0 +1,19 @@
+package com.apnacollege;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //Input
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int div = a / b;
+        System.out.println(div);
+    }
+}
+
+//   output      
+//    a ---> 8
+//    b ---> 8
+//  div ---> 0
