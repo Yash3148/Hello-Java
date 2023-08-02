64d9a20cb71d74524d54e80298090e7d281f4266
Yash Singh <122292102+Yash3148@users.noreply.github.com>
2023-08-02 21:38:37 +0530

    Update Practice.java

--- Practice.java
+++ Practice.java
@@ -4,7 +4,7 @@ public class MultiplicationTable {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("HELLO LINUX");
+        System.out.println("HELLO YS07");
 
         int a = sc.nextInt();
         int b = sc.nextInt();
