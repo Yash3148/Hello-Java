6c87b129f27db5add3ac70b2b86bfce2a97210e8
Yash Singh <122292102+Yash3148@users.noreply.github.com>
2023-08-02 12:41:38 +0530

    Create string.java

---
+++ Input.java/string.java
@@ -0,0 +1,44 @@
+package apnacollege.com;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.print(name);
+    }
+}
+
+//        ---> Yoshita
+// output ---> Yoshita
+----------------------------------------------------------------------
+package apnacollege.com;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.print(name);
+    }
+}
+
+//        ---> Tony Strak
+// output ---> Tony
+----------------------------------------------------------------------
+package apnacollege.com;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.print(name);
+    }
+}
+
+//        ---> Tony Strak
+// output ---> Tony Strak
