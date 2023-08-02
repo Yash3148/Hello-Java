5ff149d09f64ae66d8309c08d4ef7b3fe52ed4ee
Yash Singh <122292102+Yash3148@users.noreply.github.com>
2023-08-02 11:19:00 +0530

    Create sub.java

---
+++ Input.java/sub.java
@@ -0,0 +1,19 @@
+package com.apnacollege;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //Input
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sub = a - b;
+        System.out.println(sub);
+    }
+}
+
+// output
+// a ---> 5
+// b ---> 2
+// sub -> 3
