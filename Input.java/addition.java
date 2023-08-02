03c6a5ebf2be104d4f0948dce8894c3b93fbc937
Yash Singh <122292102+Yash3148@users.noreply.github.com>
2023-08-02 11:10:06 +0530

    Create addition.java

---
+++ Input.java/addition.java
@@ -0,0 +1,17 @@
+package com.apnacollege;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //Input
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);
+
+//   output      
+//    a ---> 8
+//    b ---> 8
+//  sum ---> 16
