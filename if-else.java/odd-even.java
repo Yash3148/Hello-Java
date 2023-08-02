9831254156bdf4ec36b4e920c70b306218df154d
Yash Singh <122292102+Yash3148@users.noreply.github.com>
2023-08-02 14:34:34 +0530

    odd-even.java

---
+++ if-else.java/odd-even.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the Number:");
+        int x = sc.nextInt();
+
+        if (x%2==0){
+            System.out.print("Even");
+        }else {
+            System.out.print("Odd");
+        }
+    }
+}
+
+// Enter the Number:42
+// output      ---> Even
+// Enter the Number:5
+// output      ---> odd
