1b3f1cfcdbbe553db57bbc83504c1bcf7b36f4b9
Yash Singh <122292102+Yash3148@users.noreply.github.com>
2023-08-02 14:19:07 +0530

    Create example.java

---
+++ if-else.java/example.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the correct age:");
+        int age = sc.nextInt();
+
+        if (age>18){
+            System.out.print("Adult");
+        }else {
+            System.out.print("Not Adult");
+        }
+    }
+}
+//             Enter the correct age:21
+// output ---> Adult
+//             Enter the correct age:11
+//             Not Adult           
