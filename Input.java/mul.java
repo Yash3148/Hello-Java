4b7e99c3d461ed813c0cddbc05abbd6ff861632e
Yash Singh <122292102+Yash3148@users.noreply.github.com>
2023-08-02 11:15:50 +0530

    Create mul.java

---
+++ Input.java/mul.java
@@ -0,0 +1,19 @@
+package com.apnacollege;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //Input
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int mul = a * b;
+        System.out.println(mul);
+    }
+}
+
+//   output      
+//    a ---> 8
+//    b ---> 8
+//  mul ---> 64
