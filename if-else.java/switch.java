862f3c915238ded88c6358607df6eab67bc2f8b2
Yash Singh <122292102+Yash3148@users.noreply.github.com>
2023-08-03 00:35:07 +0530

    Create switch.java

---
+++ if-else.java/switch.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.util.*;
+
+public class custom {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number:");
+
+        int button = sc.nextInt();
+
+        switch(button){
+            case 1 :
+                System.out.println("Hello");
+                break;
+            case 2 :
+                System.out.println("Nameste");
+                break;
+            case 3 :
+                System.out.println("Bonjour");
+                break;
+            default:
+                System.out.print("Invalid");
+        }
+    }
+}
+// Enter the number: 1
+// output       ---> Hello
