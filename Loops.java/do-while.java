826da4acf716de9258581d85f91c540e941c9093
Yash Singh <122292102+Yash3148@users.noreply.github.com>
2023-08-03 22:24:27 +0530

    Create do-while.java

---
+++ Loops.java/do-while.java
@@ -0,0 +1,14 @@
+import org.w3c.dom.css.Counter;
+
+public class Loops {
+    public static void main(String[] args) {
+       int i = 0;
+       do{
+           System.out.println(i);
+           i = i + 1;
+        }while (i<3);
+    }
+}
+// output ---> 0
+//             1
+//             2
