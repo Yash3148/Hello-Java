8bd1e1a60b2659e6d5f1c4eeed06603e79836d27
Yash Singh <122292102+Yash3148@users.noreply.github.com>
2023-08-03 22:35:58 +0530

    Update do-while.java

--- Loops.java/do-while.java
+++ Loops.java/do-while.java
@@ -12,3 +12,20 @@ public class Loops {
 // output ---> 0
 //             1
 //             2
+--------------------------------------------------------------------------------------------------------
+import org.w3c.dom.css.Counter;
+
+public class Loops {
+    public static void main(String[] args) {
+       int i = 12;
+       while (i<11){
+           System.out.println("Hello YS07");
+       }
+       do{
+           System.out.print("Yoshita");
+       }while (i<11);
+    }
+}
+// output ---> Yoshita
+// do will print it because System.out.print("Yoshita") is first stage in loop, then it will check condition in while (i<11); then it will stop.
+//this is called as false conditiom.
