404dbf4701c6d1a37e9ba1e35b55640cda2afe4b
Yash Singh <122292102+Yash3148@users.noreply.github.com>
2023-08-03 20:19:03 +0530

    Update for.java

--- Loops.java/for.java
+++ Loops.java/for.java
@@ -11,3 +11,52 @@ public class Loops {
 //output ---> Hello YS07
 //            Hello YS07
 //            Hello YS07
+---------------------------------------------------------------------------------------
+import org.w3c.dom.css.Counter;
+
+public class Loops {
+    public static void main(String[] args) {
+        for (int counter = 0; counter < 11; counter++ ) {
+            //counter++ ---> counter = counter + 1
+            System.out.println(counter);
+        }
+    }
+}
+// output ---> 0
+//             1
+//             2
+//             3
+//             4
+//             5
+//             6
+//             7
+//             8
+//             9
+//             10
+---------------------------------------------------------------------------------------
+import org.w3c.dom.css.Counter;
+
+public class Loops {
+    public static void main(String[] args) {
+        for (int counter = 0; counter < 11; counter++ ) {
+            //counter++ ---> counter = counter + 1
+            System.out.print(counter);
+        }
+    }
+}
+//output  ---> 012345678910
+---------------------------------------------------------------------------------------
+import org.w3c.dom.css.Counter;
+
+public class Loops {
+    public static void main(String[] args) {
+        for (int counter = 0; counter < 11; counter++ ) {
+            //counter++ ---> counter = counter + 1
+            System.out.print(counter+" ");
+        }
+    }
+}
+// output ---> 0 1 2 3 4 5 6 7 8 9 10 
+---------------------------------------------------------------------------------------
+
+
