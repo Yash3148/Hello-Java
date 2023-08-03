bc5a3ae9bc774025f5643dd91508f5a3e008dfe0
Yash Singh <122292102+Yash3148@users.noreply.github.com>
2023-08-03 19:55:20 +0530

    Create for.java

---
+++ Loops.java/for.java
@@ -0,0 +1,13 @@
+import org.w3c.dom.css.Counter;
+
+public class Loops {
+    public static void main(String[] args) {
+        for (int counter = 0; counter < 3; counter = counter + 1) {
+            System.out.println("Hello YS07");
+        }
+    }
+}
+
+//output ---> Hello YS07
+//            Hello YS07
+//            Hello YS07
