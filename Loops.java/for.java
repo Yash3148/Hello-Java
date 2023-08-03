f8069799d7977416ac3ed4ca76be64808d754ad2
Yash Singh <122292102+Yash3148@users.noreply.github.com>
2023-08-03 22:11:34 +0530

    Update for.java

--- Loops.java/for.java
+++ Loops.java/for.java
@@ -58,5 +58,19 @@ public class Loops {
 }
 // output ---> 0 1 2 3 4 5 6 7 8 9 10 
 ---------------------------------------------------------------------------------------
+import org.w3c.dom.css.Counter;
 
+public class Loops {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++){
+            System.out.println(i);
+        }
+
+    }
+}
+// output ---> 0
+//             1
+//             2
+---------------------------------------------------------------------------------------
+    
 
